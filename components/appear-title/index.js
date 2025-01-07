1a7c8544f697362a1f62cc97648d1550468a71d8
Solly922 <solomon.schuster01@gmail.com>
2025-01-06 21:09:54 -0700

    ?

--- components/appear-title/index.js
+++ components/appear-title/index.js
@@ -1,12 +1,8 @@
 import { useMediaQuery, useRect } from '@darkroom.engineering/hamo'
 import cn from 'clsx'
-import { gsap } from 'gsap'
-import { SplitText } from 'gsap/dist/SplitText'
 import { useEffect, useRef, useState } from 'react'
 import { useIntersection, useWindowSize } from 'react-use'
 import s from './appear-title.module.scss'
-
-gsap.registerPlugin(SplitText)
 
 export function AppearTitle({ children, visible = true }) {
   const el = useRef()
@@ -29,25 +25,6 @@ export function AppearTitle({ children, visible = true }) {
 
   useEffect(() => {
     if (isMobile === false) {
-      const splitted = new SplitText(el.current, {
-        type: 'lines',
-        lineThreshold: 0.3,
-        tag: 'span',
-        linesClass: s.line,
-      })
-
-      splitted.lines.forEach((line, i) => {
-        line.style.setProperty('--i', i)
-        const html = line.innerHTML
-        line.innerHTML = ''
-        const content = document.createElement('span')
-        content.innerHTML = html
-        line.appendChild(content)
-      })
-
-      return () => {
-        splitted.revert()
-      }
     }
   }, [width, rect, isMobile])
 
